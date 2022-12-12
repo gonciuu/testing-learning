fed9ed140319ac2cc8a5a0f4f23276bc4267505a
Kacper Wojak <k.wojak@medicalgorithmics.com>
2022-12-12 20:11:22 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:testing-library/react",
+    "next/core-web-vitals",
+    "next",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+  },
+};
