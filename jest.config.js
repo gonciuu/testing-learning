a2e149c8b15a8b8cbbc1f4b21d587b3458f7dae8
Kacper Wojak <k.wojak@medicalgorithmics.com>
2022-12-12 20:24:46 +0100

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,14 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
 
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
